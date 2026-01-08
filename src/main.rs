c68e53b51570ce68c31dbcbcc025408ef15d41cc
Zhengqun Koo <zhengqun.koo@gmail.com>
2026-01-08 12:44:13 +0800

    add command parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
+    let mut console_input = String::new();
+    let command_list: Vec<&str> = vec![];
     print!("$ ");
     io::stdout().flush().unwrap();
+    let _ = io::stdin().read_line(&mut console_input);
+    let command = console_input.trim();
+    if command_list.contains(&command) {
+        println!("Command found: {}", command);
+    } else {
+        println!("{}: command not found", command);
+    }
 }
