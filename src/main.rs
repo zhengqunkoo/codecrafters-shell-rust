9f62c040f212052b7240c45e4556b9a7614b3ca1
zhengqunkoo <root@zhengqunkoo.com>
2026-01-19 00:32:35 +0800

    Check executables in PATH when users use the 'type' command
    But code behaves unexpectedly in this case:
    $ type
     is /home/koo/.nvm/versions/node/v20.19.5/bin/

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #[allow(unused_imports)]
+use std::env;
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
+    // Or handle it more gracefully
+    let path_env = env::var("PATH").unwrap_or_else(|_| String::new());
+
     let command_list: Vec<&str> = vec!["exit", "echo", "type"];
     while true {
         print!("$ ");
@@ -18,7 +23,23 @@ fn main() {
                     "type" => if command_list.contains(&args) {
                         println!("{} is a shell builtin", args);
                     } else {
-                        println!("{}: not found", args);
+                        let path_dirs = path_env.split(':');
+                        let mut found = false;
+                        for path_dir in path_dirs {
+                            let full_path = std::path::Path::new(path_dir).join(args);
+                            if full_path.exists() {
+                                if let Ok(metadata) = full_path.metadata() {
+                                    if metadata.permissions().mode() & 0o111 != 0 { // if any execute bit is set
+                                        println!("{} is {}", args, full_path.display());
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        if !found {
+                            println!("{}: not found", args);
+                        }
                     },
                     _ => {}
                 }
