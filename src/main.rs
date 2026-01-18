54250b6379d4fe9249aabf0f09447851d8a47de1
zhengqunkoo <root@zhengqunkoo.com>
2026-01-19 00:06:04 +0800

    Revert previous rewrite of command processing, and add type command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
+    let command_list: Vec<&str> = vec!["exit", "echo", "type"];
     while true {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -10,8 +11,18 @@ fn main() {
         console_input = console_input.trim().to_string();
         let (command, args) = console_input.split_once(' ').unwrap_or((&console_input, ""));
         match command {
-            "exit" => break,
-            "echo" => println!("{}", args),
+            cmd if command_list.contains(&cmd) => {
+                match cmd {
+                    "exit" => break,
+                    "echo" => println!("{}", args),
+                    "type" => if command_list.contains(&args) {
+                        println!("{} is a shell builtin", args);
+                    } else {
+                        println!("{}: not found", args);
+                    },
+                    _ => {}
+                }
+            }
             _ => println!("{}: command not found", command),
         }
     }
