3c93b17ec17a356827eeedeb13dff99967eaa3cf
zhengqunkoo <root@zhengqunkoo.com>
2026-01-18 23:40:00 +0800

    Loop to truly REPL

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    let mut console_input = String::new();
     let command_list: Vec<&str> = vec![];
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let _ = io::stdin().read_line(&mut console_input);
-    let command = console_input.trim();
-    if command_list.contains(&command) {
-        println!("Command found: {}", command);
-    } else {
-        println!("{}: command not found", command);
+    while true {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut console_input = String::new();
+        let _ = io::stdin().read_line(&mut console_input);
+        let command = console_input.trim();
+        if command_list.contains(&command) {
+            println!("Command found: {}", command);
+        } else {
+            println!("{}: command not found", command);
+        }
     }
 }
