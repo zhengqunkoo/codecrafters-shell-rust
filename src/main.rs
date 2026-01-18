e7917105d62774abceff82f63c7b34e2a9c30184
zhengqunkoo <root@zhengqunkoo.com>
2026-01-18 23:58:25 +0800

    Add echo command, and rewrite processing of commands

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    let command_list: Vec<&str> = vec!["exit"];
     while true {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut console_input = String::new();
         let _ = io::stdin().read_line(&mut console_input);
-        let command = console_input.trim();
-        if command_list.contains(&command) {
-            if command == "exit" {
-                break;
-            }
-        } else {
-            println!("{}: command not found", command);
+        console_input = console_input.trim().to_string();
+        let (command, args) = console_input.split_once(' ').unwrap_or((&console_input, ""));
+        match command {
+            "exit" => break,
+            "echo" => println!("{}", args),
+            _ => println!("{}: command not found", command),
         }
     }
 }
