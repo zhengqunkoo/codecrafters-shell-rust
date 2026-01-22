11b7fa62defd8e8c15e6904947c747f2a64de00a
zhengqunkoo <root@zhengqunkoo.com>
2026-01-22 17:24:24 +0800

    Many feature commit
    - move arg parsing to before command processing
    - `type` command: run the command on each of the args and accumulate each output
    - `cmd` command: print error if too many arguments
    - `cmd` command: change directory to HOME if there are no arguments
    - ignore empty input
    - implement stdout redirection feature after command processing

--- src/main.rs
+++ src/main.rs
@@ -37,14 +37,25 @@ fn parse_args(args: &str) -> Vec<String> {
 }
 
 fn main() {
-    let command_list: Vec<&str> = vec!["exit", "echo", "type", "pwd", "cd"];
+    let command_list: Vec<String> = vec!["exit", "echo", "type", "pwd", "cd"].into_iter().map(String::from).collect();
     while true {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut console_input = String::new();
         let _ = io::stdin().read_line(&mut console_input);
         console_input = console_input.trim().to_string();
-        let (command, args) = console_input.split_once(' ').unwrap_or((&console_input, ""));
+        let (command, rest) = console_input.split_once(' ').unwrap_or((&console_input, ""));
+        let (args, filename) = if let Some((a, f)) = rest.split_once("1>") {
+            (parse_args(a), f.trim())
+        } else if let Some((a, f)) = rest.split_once('>') {
+            (parse_args(a), f.trim())
+        } else {
+            (parse_args(rest), "")
+        };
+
+        // `string_for_stdout`` will either be printed to the console, or written to `filename`.
+        // Errors are printed to the console directly.
+        let mut string_for_stdout = String::new();
         match command {
             "exit" => break,
             "echo" => {
@@ -53,50 +64,82 @@ fn main() {
                 // echo hello    world	hello world	Consecutive spaces are collapsed unless quoted.
                 // echo 'hello''world'	helloworld	Adjacent quoted strings 'hello' and 'world' are concatenated.
                 // echo hello''world	helloworld	Empty quotes '' are ignored.
-                let parsed_args = parse_args(args);
-                println!("{}", parsed_args.join(" "));
+                string_for_stdout = args.join(" ") + "\n";
             },
-            "type" => if command_list.contains(&args) {
-                println!("{} is a shell builtin", args);
-            } else {
-                if let Some(full_path) = find_executable_in_path(args) {
-                    println!("{} is {}", args, full_path.display());
+            "type" => for arg in args {
+                if command_list.contains(&arg) {
+                    string_for_stdout.push_str(&format!("{} is a shell builtin\n", arg));
+                } else if let Some(full_path) = find_executable_in_path(&arg) {
+                    string_for_stdout.push_str(&format!("{} is {}\n", arg, full_path.display()));
                 } else {
-                    println!("{}: not found", args);
+                    string_for_stdout.push_str(&format!("kooshell: type: {}: not found\n", arg));
                 }
             },
             "pwd" => {
                 match env::current_dir() {
-                    Ok(path) => println!("{}", path.display()),
+                    Ok(path) => string_for_stdout = path.display().to_string(),
                     Err(e) => println!("pwd: error retrieving current directory: {}", e),
                 }
             },
             "cd" => {
-                let target_dir = if args == "~" {
-                    env::var("HOME").unwrap_or_else(|_| String::new())
-                } else {
-                    args.to_string()
-                };
+                if args.len() > 1 {
+                    println!("cd: too many arguments");
+                    continue;
+                }
+                let target_dir = if args.len() == 0 || args[0] == "~" {
+                        env::var("HOME").unwrap_or_else(|_| String::new())
+                    } else {
+                        args[0].to_string()
+                    };
                 if let Err(_) = env::set_current_dir(&target_dir) { // this also handles relative paths
                     println!("cd: {}: No such file or directory", target_dir);
                 }
             },
+            "" => continue, // empty input, just reprompt
             _ => if let Some(full_path) = find_executable_in_path(command) {
                 let executable = full_path.file_name().unwrap(); // only the file name
-                let args = parse_args(args);
-                let status = std::process::Command::new(executable)
-                    .args(args)
-                    .status();
+                let mut cmd = std::process::Command::new(executable);
+                cmd.args(args);
+
+                if !filename.is_empty() {
+                    if let Ok(file) = std::fs::File::create(filename) {
+                        cmd.stdout(file);
+                    }
+                }
+
+                let status = cmd.status();
                 match status {
                     Ok(status) => {
                         if !status.success() {
-                            println!("{}: exited with status {}", command, status);
+                            //println!("{}: exited with status {}", command, status);
                         }
                     }
                     Err(e) => println!("{}: failed to execute: {}", command, e),
                 }
             } else {
                 println!("{}: command not found", command);
+            }
+        }
+
+        // Handle filename. Open file descriptor
+        if filename.is_empty() {
+            // No output redirection, print to console
+            print!("{}", string_for_stdout);
+        } else {
+            // Redirect output to file
+            let stdout_file = std::fs::OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open(filename);
+            
+            match stdout_file {
+                Ok(mut file) => {
+                    write!(file, "{}", string_for_stdout).unwrap();
+                },
+                Err(_) => {
+                    println!("{}: cannot open file for output redirection", filename);
+                }
             }
         }
     }
